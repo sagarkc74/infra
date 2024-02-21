32e72fa2a8147cd0aebcaecc82b1d12002232ca9
Sagar Khadka <145722229+sagarkc74@users.noreply.github.com>
2024-02-21 16:55:05 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
-
+terraform {
+  backend "s3" {
+    bucket = "sagar-terraform-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state"
+    encrypt = true
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
